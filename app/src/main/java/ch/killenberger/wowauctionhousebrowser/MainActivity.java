5b1bf98b01ac8fcd797bfe816cf606d1d2baf48c
Tim Killenberger <timkillenberger@gmail.com>
2021-12-27 07:32:01 +0100

    Fetch improvements

--- app/src/main/java/ch/killenberger/wowauctionhousebrowser/MainActivity.java
+++ app/src/main/java/ch/killenberger/wowauctionhousebrowser/MainActivity.java
@@ -1,12 +1,10 @@
 package ch.killenberger.wowauctionhousebrowser;
 
 import android.Manifest;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.database.sqlite.SQLiteDatabase;
-import android.os.Build;
 import android.os.Bundle;
-import android.provider.ContactsContract;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
@@ -33,8 +31,10 @@ import ch.killenberger.wowauctionhousebrowser.model.ApplicationSettings;
 import ch.killenberger.wowauctionhousebrowser.model.Realm;
 import ch.killenberger.wowauctionhousebrowser.model.UserSettings;
 import ch.killenberger.wowauctionhousebrowser.service.ConnectedRealmService;
-import ch.killenberger.wowauctionhousebrowser.service.ItemClassUpdateService;
 import ch.killenberger.wowauctionhousebrowser.service.ItemClassService;
+import ch.killenberger.wowauctionhousebrowser.service.ItemClassUpdateService;
+import ch.killenberger.wowauctionhousebrowser.service.ItemMediaService;
+import ch.killenberger.wowauctionhousebrowser.service.ItemMediaUpdateService;
 import ch.killenberger.wowauctionhousebrowser.service.ItemService;
 import ch.killenberger.wowauctionhousebrowser.service.ItemSubClassService;
 import ch.killenberger.wowauctionhousebrowser.service.ItemSubClassUpdateService;
@@ -42,6 +42,7 @@ import ch.killenberger.wowauctionhousebrowser.service.ItemUpdateService;
 import ch.killenberger.wowauctionhousebrowser.service.OAuth2Service;
 import ch.killenberger.wowauctionhousebrowser.service.RealmService;
 import ch.killenberger.wowauctionhousebrowser.sqlite.DatabaseHelper;
+import ch.killenberger.wowauctionhousebrowser.util.AlertUtil;
 
 public class MainActivity extends AppCompatActivity {
     private static final String DB_DIR  = "databases";
@@ -50,13 +51,12 @@ public class MainActivity extends AppCompatActivity {
     private AutoCompleteTextView realmInput;
     private Spinner              regionSpinner;
     private Button searchButton;
-    private Button resetButton;
 
     private UserSettings        userSettings        = UserSettings.getInstance();
     private ApplicationSettings applicationSettings = ApplicationSettings.getInstance();
     private List<Realm>         realms              = new ArrayList<>();
-    private ArrayAdapter<Realm> adapter;
 
+    private boolean initialization = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -72,80 +72,101 @@ public class MainActivity extends AppCompatActivity {
             copyDatabse();
         }
 
-        // CREATE API ACCESS TOKEN
-        try {
-            ApplicationSettings.getInstance().setAccessToken(new OAuth2Service().execute().get());
-        } catch (ExecutionException | InterruptedException e) {
-            e.printStackTrace();
-        }
-
-        try {
-            fetchDataIfNecessary();
-        } catch (ExecutionException | InterruptedException e) {
-            e.printStackTrace();
-        }
-
         this.regionSpinner = findViewById(R.id.regionSpinner);
         this.realmInput    = findViewById(R.id.realmInput);
         this.searchButton  = findViewById(R.id.searchButton);
-        this.resetButton  = findViewById(R.id.resetDatabaseButton);
-        this.adapter       = new ArrayAdapter<>(this, android.R.layout.simple_dropdown_item_1line, realms);
 
         // SETUP ADAPTERS
-        this.realmInput.setAdapter(adapter);
+        this.realmInput.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_dropdown_item_1line, realms));
         this.regionSpinner.setAdapter(new ArrayAdapter<>(this, R.layout.support_simple_spinner_dropdown_item, Region.values()));
-        this.regionSpinner.setOnItemSelectedListener(createItemSelectedListener());
 
-        this.searchButton.setOnClickListener(createRealmOnClickListener());
-        this.resetButton.setOnClickListener(createResetDbnClickListener());
+        // SETUP LISTENERS
+        this.regionSpinner.setOnItemSelectedListener(createItemSelectedListener());
+        this.searchButton.setOnClickListener(createSearchOnClickListener());
+
+        // CREATE API ACCESS TOKEN
+        try {
+            new OAuth2Service(this).execute().get();
+        } catch (ExecutionException | InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        // NOTIFY USER IF AUTHENTICATION HAS FAILED
+        if(ApplicationSettings.getInstance().getAccessToken() == null) {
+            AlertUtil.createAlertDialog(this, "Authorization", "Unable to authorize against the Blizzard API.\nPlease restart the application!", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    finish();
+                    System.exit(0);
+                }
+            });
+        } else {
+            try {
+                fetchDataIfNecessary();
+            } catch (ExecutionException | InterruptedException e) {
+                return;
+            }
+        }
+
+        this.initialization = false;
     }
 
     private void fetchDataIfNecessary() throws ExecutionException, InterruptedException {
-        final boolean itemClassUpdateRequired = new ItemClassUpdateService().execute().get();
+        final boolean itemClassUpdateRequired = new ItemClassUpdateService(this).execute().get();
         if(itemClassUpdateRequired) {
             fetchItemClasses();
         }
 
-        final boolean itemSubClassUpdateRequired = new ItemSubClassUpdateService().execute().get();
+        final boolean itemSubClassUpdateRequired = new ItemSubClassUpdateService(this).execute().get();
         if(itemSubClassUpdateRequired) {
             fetchItemSubClasses();
         }
 
-        final boolean itemUpdateRequired = new ItemUpdateService().execute().get();
+        final boolean itemUpdateRequired = new ItemUpdateService(this).execute().get();
         if(itemUpdateRequired) {
             fetchItems();
         }
+
+        final boolean itemMediaUpdateRequired = new ItemMediaUpdateService().execute().get();
+        if(itemMediaUpdateRequired) {
+            fetchItemMedia();
+        }
     }
 
-    private void fetchItemClasses() throws ExecutionException, InterruptedException {
-        System.out.println("Creating Item Classes...");
-        new ItemClassService().execute().get();
+    private void fetchItemClasses() {
+        System.out.println("Fetching Item Classes...");
+        new ItemClassService(this).execute();
     }
 
-    private void fetchItemSubClasses() throws ExecutionException, InterruptedException {
-        System.out.println("Creating Item SubClasses...");
-        new ItemSubClassService().execute().get();
+    private void fetchItemSubClasses() {
+        System.out.println("Fetching Item SubClasses...");
+        new ItemSubClassService(this).execute();
     }
 
     private void fetchItems() {
-        System.out.println("Creating Items...");
+        System.out.println("Fetching Items...");
         new ItemService(this).execute();
+    }
+
+    private void fetchItemMedia() {
+        System.out.println("Fetching Item Media...");
+        new ItemMediaService(this).execute();
     }
 
     private AdapterView.OnItemSelectedListener createItemSelectedListener() {
        return new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                if(initialization) {
+                    return;
+                }
+
                 userSettings.setRegion((Region) parent.getItemAtPosition(position));
 
                 try {
-                    realms = new RealmService().execute().get();
-
-                    adapter.clear();
-                    adapter.addAll(realms);
-                } catch (ExecutionException e) {
-                    e.printStackTrace();
-                } catch (InterruptedException e) {
+                    realms = new RealmService(MainActivity.this).execute().get();
+                    realmInput.setAdapter(new ArrayAdapter<Realm>(MainActivity.this, android.R.layout.simple_dropdown_item_1line, realms));
+                } catch (ExecutionException | InterruptedException e) {
                     e.printStackTrace();
                 }
             }
@@ -157,7 +178,7 @@ public class MainActivity extends AppCompatActivity {
         };
     }
 
-    private View.OnClickListener createRealmOnClickListener() {
+    private View.OnClickListener createSearchOnClickListener() {
         return new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -168,42 +189,24 @@ public class MainActivity extends AppCompatActivity {
                         userSettings.setRealm(r);
 
                         try {
-                            userSettings.setConnectedRealmId(new ConnectedRealmService().execute().get());
-                        } catch (ExecutionException e) {
-                            e.printStackTrace();
-                        } catch (InterruptedException e) {
-                            e.printStackTrace();
+                            userSettings.setConnectedRealmId(new ConnectedRealmService(MainActivity.this).execute().get());
+
+                            Intent intent = new Intent(getBaseContext(), AuctionsActivity.class);
+                            startActivity(intent);
+                        } catch (ExecutionException | InterruptedException e) {
+                            AlertUtil.createAlertDialog(MainActivity.this, "Oops", getString(R.string.connection_failed_error_msg));
                         }
 
-                        break;
+                        return;
                     }
                 }
 
-                Intent intent = new Intent(getBaseContext(), AuctionsActivity.class);
-                startActivity(intent);
-            }
-        };
-    }
-
-    private View.OnClickListener createResetDbnClickListener() {
-        return new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                DatabaseHelper db = new DatabaseHelper(MainActivity.this);
-                db.resetDatabase();
-                db.close();;
-
-                try {
-                    fetchDataIfNecessary();
-                } catch (ExecutionException | InterruptedException e) {
-                    e.printStackTrace();
-                }
+                AlertUtil.createAlertDialog(MainActivity.this, "Invalid Realm", "You've entered an invalid realm.\nPlease select a realm from the dropdown.");
             }
         };
     }
 
     public void checkPermissions() {
-        // Here, thisActivity is the current activity
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE}, 1);
         }
@@ -217,7 +220,7 @@ public class MainActivity extends AppCompatActivity {
         final String appDataPath  = this.getApplicationInfo().dataDir;
 
         //Make sure the /databases folder exists
-        File dbFolder = new File(appDataPath + "/" + DB_DIR);
+        final File dbFolder = new File(appDataPath + "/" + DB_DIR);
         dbFolder.mkdir();//This can be called multiple times.
 
         File dbFilePath = new File(appDataPath + "/" + DB_DIR + "/" + DB_NAME);
