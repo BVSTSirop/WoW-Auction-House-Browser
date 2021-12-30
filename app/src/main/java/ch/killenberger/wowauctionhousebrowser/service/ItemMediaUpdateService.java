9f92a77c3405557745e4dac60f9843c6685b99bf
Tim Killenberger <timkillenberger@gmail.com>
2021-12-30 16:27:43 +0100

    Revert "Implemented threading concept for image download"
    This reverts commit 2086a80ecf69ef2169f528ec4a0215d7a1f2fac3.

--- app/src/main/java/ch/killenberger/wowauctionhousebrowser/service/ItemMediaUpdateService.java
+++ app/src/main/java/ch/killenberger/wowauctionhousebrowser/service/ItemMediaUpdateService.java
@@ -18,13 +18,15 @@ import ch.killenberger.wowauctionhousebrowser.sqlite.DatabaseHelper;
 
 public class ItemMediaUpdateService extends AsyncTask<String, Integer, Boolean> {
 
+    private static final int[] NO_MEDIA_ITEMS = new int[]{58170, 111076, 114960, 115603, 115854, 117583, 117784, 118226, 119809, 119811, 119812, 119816, 119818, 119820, 122334, 130318, 137675, 159894, 159895, 161973, 161974, 174800};
+
     @Override
     protected Boolean doInBackground(String... strings) {
         final DatabaseHelper db             = new DatabaseHelper(ApplicationSettings.getInstance().getApplicationContext());
         final int            nrMissingMedia = db.getMissingItemMediaIds().size();
         db.close();
 
-        if(nrMissingMedia == 0) {
+        if(nrMissingMedia - NO_MEDIA_ITEMS.length == 0) {
             return false;
         }
 
