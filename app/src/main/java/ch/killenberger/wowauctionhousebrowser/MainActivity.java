6670c70e3a550d84505b3c58f29d4326f5e08dbb
Tim Killenberger <timkillenberger@gmail.com>
2021-12-30 16:52:31 +0100

    Make image download optional

--- app/src/main/java/ch/killenberger/wowauctionhousebrowser/MainActivity.java
+++ app/src/main/java/ch/killenberger/wowauctionhousebrowser/MainActivity.java
@@ -51,6 +51,7 @@ public class MainActivity extends AppCompatActivity {
     private AutoCompleteTextView realmInput;
     private Spinner              regionSpinner;
     private Button searchButton;
+    private Button downloadImagesButton;
 
     private UserSettings        userSettings        = UserSettings.getInstance();
     private ApplicationSettings applicationSettings = ApplicationSettings.getInstance();
@@ -72,9 +73,10 @@ public class MainActivity extends AppCompatActivity {
             copyDatabse();
         }
 
-        this.regionSpinner = findViewById(R.id.regionSpinner);
-        this.realmInput    = findViewById(R.id.realmInput);
-        this.searchButton  = findViewById(R.id.searchButton);
+        this.regionSpinner         = findViewById(R.id.regionSpinner);
+        this.realmInput            = findViewById(R.id.realmInput);
+        this.searchButton          = findViewById(R.id.searchButton);
+        this.downloadImagesButton  = findViewById(R.id.downloadImagesButton);
 
         // SETUP ADAPTERS
         this.realmInput.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_dropdown_item_1line, realms));
@@ -83,6 +85,7 @@ public class MainActivity extends AppCompatActivity {
         // SETUP LISTENERS
         this.regionSpinner.setOnItemSelectedListener(createItemSelectedListener());
         this.searchButton.setOnClickListener(createSearchOnClickListener());
+        this.downloadImagesButton.setOnClickListener(createDownloadImagesOnClickListener());
 
         // CREATE API ACCESS TOKEN
         try {
@@ -125,11 +128,6 @@ public class MainActivity extends AppCompatActivity {
         final boolean itemUpdateRequired = new ItemUpdateService(this).execute().get();
         if(itemUpdateRequired) {
             fetchItems();
-        }
-
-        final boolean itemMediaUpdateRequired = new ItemMediaUpdateService().execute().get();
-        if(itemMediaUpdateRequired) {
-            fetchItemMedia();
         }
     }
 
@@ -202,6 +200,18 @@ public class MainActivity extends AppCompatActivity {
                 }
 
                 AlertUtil.createAlertDialog(MainActivity.this, "Invalid Realm", "You've entered an invalid realm.\nPlease select a realm from the dropdown.");
+            }
+        };
+    }
+
+    private View.OnClickListener createDownloadImagesOnClickListener() {
+        return new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final boolean itemMediaUpdateRequired = new ItemMediaUpdateService().execute().get();
+                if(itemMediaUpdateRequired) {
+                    fetchItemMedia();
+                }
             }
         };
     }
